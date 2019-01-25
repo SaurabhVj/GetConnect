4c9b30b472487899aacd7852d79e8cfccd52d177
SaurabhVj <srbhvj123@gmail.com>
2019-01-26 03:29:26 +0530

    Login and Register done

--- app/src/main/java/com/vijay/saurabh/getconnect/MainActivity.java
+++ app/src/main/java/com/vijay/saurabh/getconnect/MainActivity.java
@@ -1,13 +1,66 @@
 package com.vijay.saurabh.getconnect;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.karan.churi.PermissionManager.PermissionManager;
+import com.vijay.saurabh.getconnect.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth ;
+    FirebaseUser user ;
+
+    PermissionManager manager ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        manager = new PermissionManager() {};
+        manager.checkAndRequestPermissions(this);
+
+        user = auth.getCurrentUser();
+        if(user==null) {
+            setContentView(R.layout.activity_main);
+        }
+        else
+        {
+            Intent i = new Intent(MainActivity.this , MyNavigationActivity.class);
+            startActivity(i);
+            finish();
+        }
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        manager.checkResult(requestCode , permissions , grantResults);
+        ArrayList<String> denied_permissions = manager.getStatus().get(0).denied ;
+
+        if(denied_permissions.isEmpty())
+        {
+            Toast.makeText(this, "Permission Enabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void gotologin(View v)
+    {
+        Intent i = new Intent(MainActivity.this , LoginActivity.class);
+        startActivity(i);
+    }
+    public void gotoregister(View v)
+    {
+        Intent i = new Intent(MainActivity.this , Register.class);
+        startActivity(i);
+    }
+
 }

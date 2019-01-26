3f09959c50821e8541815e0bbb107f2d69550f48
SaurabhVj <42088766+SaurabhVj@users.noreply.github.com>
2019-01-26 06:54:35 +0530

    Add files via upload

---
+++ app/src/main/java/com/vijay/saurabh/getconnect/OfficialCircle/CircleJoin.java
@@ -0,0 +1,25 @@
+package com.vijay.saurabh.getconnect.OfficialCircle;
+
+public class CircleJoin {
+
+    String circlenumberid ;
+    public CircleJoin()
+    {
+
+    }
+
+    public CircleJoin(String circlenumberid)
+    {
+      this.circlenumberid = circlenumberid ;
+
+    }
+
+    public String getCirclenumberid() {
+        return circlenumberid;
+    }
+
+    public void setCirclenumberid(String circlenumberid) {
+        this.circlenumberid = circlenumberid;
+    }
+}
+

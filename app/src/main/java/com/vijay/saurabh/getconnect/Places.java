7b899d2cdfa22b8ab3c139d6bf2e10684d023c65
jankit311 <jankit311@gmail.com>
2019-01-26 06:27:29 +0530

    Places Created

---
+++ app/src/main/java/com/vijay/saurabh/getconnect/Places.java
@@ -0,0 +1,13 @@
+package com.vijay.saurabh.getconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Places extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
